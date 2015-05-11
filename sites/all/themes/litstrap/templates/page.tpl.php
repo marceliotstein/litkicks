28748800c82dcc41f600988d4a10fdfb17a8ab1b
Marc Eliot Stein <marc.eliot.stein@gmail.com>
2015-05-11 19:52:24 -0400

    Initial stopping point for Litkicks mobile-first redesign May 2015
    Still plenty of work to do!

--- sites/all/themes/litstrap/templates/page.tpl.php
+++ sites/all/themes/litstrap/templates/page.tpl.php
@@ -99,11 +99,13 @@ $themeimgbase = $themebase . "/images";
     <!-- end of header -->
 
     <div class="row lkmain">
-      <div class="col-xs-3 col-sm-3 col-md-2 col-lg-2 col-xl-2 lksideleft centered-text">
-        <?php print render($page['sidebar_first']); ?>
+      <div class="hidden-xs hidden-sm col-md-3 col-lg-3 col-xl-3">
+        <div class="lksideleft">
+          <?php print render($page['sidebar_first']); ?>
+        </div>
       </div>  <!-- /#sidebar-first -->
 
-      <div class="col-xs-9 col-sm-9 col-md-7 col-lg-7 col-xl-7 lkcore">
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 lkcore">
         <div class="lkcore-inner">
           <a id="main-content"></a>
 
